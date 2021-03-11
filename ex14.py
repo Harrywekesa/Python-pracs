5079b195a6bfa59e7d2cad4e277e0d572fe952d4
Pipoporopipopopipo Wekesa <harrisonwekesa@gmail.com>
2021-03-11 07:19:46 +0300

    More python practicals

---
+++ ex14.py
@@ -0,0 +1,20 @@
+the_count =[1,2,3,4,5] 
+fruits =['apples','oranges','pears','apricots'] 
+change =[1,'pennies',2,'dimes',3,'quarters']
+
+for number in the_count:
+    print(f"This is count:{number}")
+    
+for fruit in fruits:
+    print(f"This are the fruits:{fruit}")
+    
+for i in change:
+    print(f"I got {i}")
+    
+elements = []    
+for i in range(1, 6):
+    print(f"Adding {i} to the list")
+    elements.append(i)
+    
+for i in elements:
+    print(f"Elements was: {i}")
